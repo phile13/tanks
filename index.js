ce631ffda5ca4d14957dea7ca17839d648c6cdd5
phile13 <erick.difiore@gmail.com>
2023-07-14 16:06:46 -0400

    removed execss code

--- index.js
+++ index.js
@@ -7,42 +7,8 @@ server.on("open", () => {
 	console.log("opened");
 });
 
-let client_id = 1;
-let clients = {};
 server.on("connection", (client) => {
 	console.log("connection");
 	let player = new Player(client);
-
-	client.on("message", (msg) => {
-		let req = null;
-		try{
-			req = JSON.parse(msg);
-		} catch(e){
-			req = msg;
-		}
-
-		if("id" in req && "action" in req){
-			if(req.action == "new"){
-				clients[client_id] = new Player(client_id);
-				client.send('{"type":"new","id":'+ client_id + '}');
-				client_id++;
-			}
-			else if(req.id in clients){
-				if(req.action == "MOVE"){
-					clients[req.id].x++;
-					client.send(clients[client_id]);
-				}
-				else if(req.action == "STATE"){
-
-				}
-			}
-			else{
-				client.send("action[" + req.action + "]: unknown");
-			}
-		}
-		else{
-			client.send("json not correct" + JSON.stringify(msg));
-		}
-	});
 });
 
