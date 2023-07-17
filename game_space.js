4fa24c1b077791f9fcfec246acf89a90918c1216
phile13 <erick.difiore@gmail.com>
2023-07-17 14:36:20 -0400

    gradient map

--- game_space.js
+++ game_space.js
@@ -4,7 +4,7 @@ class GameSpace {
     static height = 600;
     static pieces = [];
 
-    static GRASS = 50;
+    static WATERLINE = 50;
     static WALL = 99;
 
     static PeakHeight(A, x0, y0, sx2, sy2, x, y) {
@@ -14,7 +14,7 @@ class GameSpace {
         let a = GameSpace.PeakHeight(P1.A, P1.x0, P1.y0, P1.sx2, P1.sy2, x, y);
         let b = GameSpace.PeakHeight(P2.A, P2.x0, P2.y0, P2.sx2, P2.sy2, x, y);
         let c = GameSpace.PeakHeight(P3.A, P3.x0, P3.y0, P3.sx2, P3.sy2, x, y);
-        return 10 * Math.floor((Math.max(a, b, c) + a + b + c - Math.min(a, b, c))/3);
+        return 1 + 25.4 * Math.floor((Math.max(a, b, c) + a + b + c - Math.min(a, b, c))/3);
     }
     static CreatePeak(x) {
         return {
@@ -29,20 +29,20 @@ class GameSpace {
         let x1 = GameSpace.width - 1;
         let x2 = GameSpace.width - 2;
         for (let y = 0; y < GameSpace.height; y++) {
-            GameSpace.board[y][0] = 99;
-            GameSpace.board[y][1] = 99;
-            GameSpace.board[y][x2] = 99;
-            GameSpace.board[y][x1] = 99;
+            GameSpace.board[y][0] = GameSpace.WALL;
+            GameSpace.board[y][1] = GameSpace.WALL;
+            GameSpace.board[y][x2] = GameSpace.WALL;
+            GameSpace.board[y][x1] = GameSpace.WALL;
         }
 
 
         let y1 = GameSpace.height - 1;
         let y2 = GameSpace.height - 2;
         for (let x = 0; x < GameSpace.width; x++) {
-            GameSpace.board[0][x] = 99;
-            GameSpace.board[1][x] = 99;
-            GameSpace.board[y2][x] = 99;
-            GameSpace.board[y1][x] = 99;
+            GameSpace.board[0][x] = GameSpace.WALL;
+            GameSpace.board[1][x] = GameSpace.WALL;
+            GameSpace.board[y2][x] = GameSpace.WALL;
+            GameSpace.board[y1][x] = GameSpace.WALL;
         }
     }
 
@@ -60,73 +60,10 @@ class GameSpace {
         for (let y = 0; y < GameSpace.height; y++) {
             for (let x = 0; x < GameSpace.width; x++) {
                 let elevation = GameSpace.CalcElevation(P1, P2, P3, x, y);
-                GameSpace.board[y][x] = (elevation < 40) ? elevation : elevation + biome;
+                GameSpace.board[y][x] = GameSpace.CalcElevation(P1, P2, P3, x, y);
             }
         }
         GameSpace.AddWalls();
-    }
-
-
-    static CreateOld(width, height) {
-        GameSpace.width = width;
-        GameSpace.height = height;
- 
-        GameSpace.board = new Array(height).fill().map(() => new Array(width).fill(0));
-
-        let num_x_tiles = width / 40;
-        let num_y_tiles = height / 40;
-        let tile_values = new Array(num_y_tiles).fill().map(() => new Array(num_x_tiles).fill(0));
-
-
-        let elevation_change_map = [-2, -1, -1, 0, 0, 0, 0, 1, 1, 2];
-        let elevation = Math.floor(10 * Math.random());
-        let biome = Math.floor(10 * Math.random());
-       
-
-        for (let y_tile = 0; y_tile < num_y_tiles; y_tile++) {
-
-            let start_y = y_tile * 40;
-            let stop_y = (y_tile + 1) * 40;
-            for (let x_tile = 0; x_tile < num_x_tiles; x_tile++) {
-                let new_elevation = elevation + elevation_change_map[Math.floor(10 * Math.random())];
-                elevation = (new_elevation >= 0 && new_elevation < 10) ? new_elevation : elevation;
-
-                if (y_tile % 10 == 0 && x_tile % 10 == 0) {
-                    biome = 1 + Math.floor(2 * Math.random());
-                }
-
-                let value = elevation * 10 + ((elevation < 4) ? 0 : biome);
-                
-
-
-                let start_x = x_tile * 40;
-                let stop_x = (x_tile + 1) * 40;
-                for (let y = start_y; y < stop_y; y++) {
-                    for (let x = start_x; x < stop_x; x++) {
-                        GameSpace.board[y][x] = value;
-                    }
-                }
-            }
-        }
-   
-        let x1 = width - 1;
-        let x2 = width - 2;
-        for (let y = 0; y < height; y++) {
-            GameSpace.board[y][0] = 99;
-            GameSpace.board[y][1] = 99;
-            GameSpace.board[y][x2] = 99;
-            GameSpace.board[y][x1] = 99;
-        }
-
-
-        let y1 = height - 1;
-        let y2 = height - 2;
-        for (let x = 0; x < width; x++) {
-            GameSpace.board[0][x] = 99;
-            GameSpace.board[1][x] = 99;
-            GameSpace.board[y2][x] = 99;
-            GameSpace.board[y1][x] = 99;
-        }
     }
 
     static BoardToString() {
@@ -147,7 +84,7 @@ class GameSpace {
 
             for (let Y = start_y; Y < stop_y; Y++) {
                 for (let X = start_x; X < stop_x; X++) {
-                    if (GameSpace.board[Y][X] == GameSpace.WALL || GameSpace.board[Y][X] < 40) {
+                    if (GameSpace.board[Y][X] == GameSpace.WALL || GameSpace.board[Y][X] < GameSpace.WATERLINE) {
                         return false;
                     }
                 }
