c9e39dc539138e274343c5c5e7371ec4c62d8ceb
phile13 <47676503+phile13@users.noreply.github.com>
2023-09-17 13:13:22 -0400

    Update player.js

--- view/player.js
+++ view/player.js
@@ -6,19 +6,19 @@ class player{
       player.keycodes = [];
       
       //numpad with numlock on and off
-      player.keycodes[36] = player.keycodes[103] = "NW";//7
-      player.keycodes[38] = player.keycodes[104] = "N"; //8 
-      player.keycodes[33] = player.keycodes[105] = "NE";//9
-      player.keycodes[39] = player.keycodes[102] = "E"; //6
-      player.keycodes[34] = player.keycodes[99] = "SE"; //3
-      player.keycodes[40] = player.keycodes[98] = "S";  //2
-      player.keycodes[35] = player.keycodes[97] = "SW"; //1
-      player.keycodes[37] = player.keycodes[100] = "W"; //4
+      player.keycodes[36] = player.keycodes[103] = "ALPHA.NW";//7
+      player.keycodes[38] = player.keycodes[104] = "ALPHA.N"; //8 
+      player.keycodes[33] = player.keycodes[105] = "ALPHA.NE";//9
+      player.keycodes[39] = player.keycodes[102] = "ALPHA.E"; //6
+      player.keycodes[34] = player.keycodes[99] = "ALPHA.SE"; //3
+      player.keycodes[40] = player.keycodes[98] = "ALPHA.S";  //2
+      player.keycodes[35] = player.keycodes[97] = "ALPHA.SW"; //1
+      player.keycodes[37] = player.keycodes[100] = "ALPHA.W"; //4
 
-      player.keycodes[65] = "L"; //a
-      player.keycodes[68] = "R"; //d
-      player.keycodes[87] = "U"; //w
-      player.keycodes[88] = "D"; //x
+      player.keycodes[65] = "BETA.L"; //a
+      player.keycodes[68] = "BETA.R"; //d
+      player.keycodes[87] = "BETA.U"; //w
+      player.keycodes[88] = "BETA.D"; //x
       
       player.keycodes[32] = "FIRE"; //spacebar
     }
