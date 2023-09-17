615696dd130f8f46a7b56a05282936a77b144950
phile13 <47676503+phile13@users.noreply.github.com>
2023-09-17 12:51:52 -0400

    Update client_messenger.js

--- utils/client_messenger.js
+++ utils/client_messenger.js
@@ -1,29 +1,8 @@
-class client_messenger{
+class client_messenger extends event_handler{
   constructor(){
     this.events = {};
   }
-  
-  AddEventListener(type,func){
-    if(type in events == false){
-      this.events[type] = [];
-    }
-    this.events[type].push(func);
-  }
 
-  DispatchEvent(type,data){
-    if(type in this.events){
-      let evts = this.events[type];
-      for(let e = 0; e < evts.length; e++){
-        try{
-          evts[e](data);
-        }
-        catch(err){
-
-        }
-      }
-    }
-  }
-  
   Init(){
     this.ws = new WebSocket("ws://74.208.107.245:32123");
     this.ws.addEventListener("open", this.OpenHandler);
