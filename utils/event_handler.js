06eb328d9abbb8fce0d4a6a722f5ff0d8d539048
phile13 <47676503+phile13@users.noreply.github.com>
2023-09-17 12:50:06 -0400

    Update event_handler.js

--- utils/event_handler.js
+++ utils/event_handler.js
@@ -2,4 +2,25 @@ class event_handler{
   constructor(){
     this.events = {};
   }
+  
+  AddEventListener(type,func){
+    if(type in events == false){
+      this.events[type] = [];
+    }
+    this.events[type].push(func);
+  }
+
+  DispatchEvent(type,data){
+    if(type in this.events){
+      let evts = this.events[type];
+      for(let e = 0; e < evts.length; e++){
+        try{
+          evts[e](data);
+        }
+        catch(err){
+
+        }
+      }
+    }
+  }
 }
