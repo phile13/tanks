96e3f1d62491ca93216754ba821a8ae5dc7eb231
phile13 <47676503+phile13@users.noreply.github.com>
2023-09-23 12:08:31 -0400

    Update client_controller.js

--- new/client_controller.js
+++ new/client_controller.js
@@ -1,5 +1,26 @@
-class client_controller{
-  constructor(){
+class client_controller{  
+  constructor(ui){
+    this.ui = ui;
+    this.id = -1;
+    this.ws = new WebSocket("ws://74.208.107.245:32123");
+    ws.addEventListener("open", this.New);
+    ws.addEventListener("message", this.Receive);
+  }
 
+  Open(){
+    this.Send("NEW" + this.ui.type);
+  }
+
+  Send(action){
+    this.ws.send(`{"id":${this.id},"action":"${action}"}`);
+  }
+
+  Receive(event){
+    try{
+      let msg = JSON.parse(event.data);
+      ui.OnServerUpdate(msg);
+    }
+    catch(e){
+    }
   }
 }
