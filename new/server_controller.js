f86c132bd7deaf01555deb487d5340434cea93d6
phile13 <47676503+phile13@users.noreply.github.com>
2023-09-23 17:18:05 -0400

    Update server_controller.js

--- new/server_controller.js
+++ new/server_controller.js
@@ -1,25 +1,23 @@
 class server_controller{
   constructor(ui){
-    this.ui = ui;
-    this.id = -1;
-    this.ws = new WebSocket("ws://74.208.107.245:32123");
+    this.ws = new WebSocket.Server({port: 32123});
     ws.addEventListener("open", this.Open);
     ws.addEventListener("message", this.Receive);
   }
 
   Open(){
-    this.Send("NEW" + this.ui.type);
+    console.log("Listening");
   }
 
-  Send(action){
-    this.ws.send(`{"id":${this.id},"action":"${action}"}`);
+  Send(obj){
+    this.ws.send(JSON.stringify(obj));
   }
 
   Receive(event){
     try{
       let msg = JSON.parse(event.data);
-      if("type" in msg and msg.type == "_GAME_UPDATE_"){
-        this.ui.board.Update(msg);
+      if("id" in msg && "action" in msg){
+        this.board.Update(msg.id, msg.action);
       }
     }
     catch(e){
